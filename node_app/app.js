9f906fac10e8a0464e92b9c74c9fa0003d4a7e48
Rtheodor <rafaeldacunhatheodoro@gmail.com>
2021-01-26 23:54:53 -0300

    Como utilizar o middlewares e validaçao se o nome existe no array

--- node_app/app.js
+++ node_app/app.js
@@ -2,20 +2,74 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Olá Mundo!");
+app.use(express.json());
+
+const contatos = ['Rafael', 'Camila', 'Adriana', 'Isis', 'Flora'];
+
+/*app.use((req, res, next) => {
+    console.log("Acessou o Middlewares!");
+    next();
+})*/
+
+function valContato(req, res, next) {
+    if (!req.body.nome) {
+        return res.status(400).json({
+            error: "Necessário enviar o nome!"
+        })
+    }
+    return next();
+}
+
+function valPosContato(req, res, next) {
+    if (!contatos[req.params.id]) {
+        return res.status(400).json({
+            error: "Contato não encontado!"
+        })
+    }
+    return next();
+}
+
+app.get("/", (req, res) => {
+    return res.json(contatos);
 });
 
-app.get("/contato/:id", (req, res) =>{
-    const {sit} = req.query;
-    const {id} = req.params;
+app.get("/contatos", (req, res) => {
+    console.log("Acessou o listar contatos!");
+    return res.json(contatos);
+});
+
+app.get("/contatos/:id", valPosContato, (req, res) => {
+    const { id } = req.params;
     return res.json({
-        id:id ,
-        nome: "Rafael",
-        sit:sit
-});
+        nome: contatos[id]
+    });
 });
 
-app.listen(8080, ()=>{
-    console.log("Servidor iniciado na porta 8080!")
+app.post("/contatos", valContato, (req, res) => {
+    const { nome } = req.body;
+
+    contatos.push(nome);
+
+    return res.json(contatos);
+});
+
+app.put("/contatos/:id", valPosContato, valContato, (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    contatos[id] = nome;
+
+    return res.json(contatos);
+});
+
+app.delete("/contatos/:id", valPosContato, (req, res) => {
+    const { id } = req.params;
+
+    contatos.splice(id, 1);
+
+    return res.json(contatos)
+
 })
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080/contatos");
+});
